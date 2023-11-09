d7aff15ffcbb2cda8d95879db0cb131a4718a70f
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-11-09 11:19:31 -0300

    modifyBack and add log out

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -4,57 +4,59 @@ import clienteAxios from "../config/clienteAxios";
 
 const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
+  const [auth, setAuth] = useState({});
+  const [cargando, setCargando] = useState(true);
 
-    const [ auth, setAuth ] = useState({});
-    const [ cargando, setCargando ] = useState(true);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  useEffect(() => {
+    const autenticarUsuario = async () => {
+      const token = localStorage.getItem("token");
 
-    useEffect(() => {
-        const autenticarUsuario = async () => {
-            const token = localStorage.getItem('token')
+      if (!token) {
+        setCargando(false);
+        return;
+      }
 
-            if (!token){
-                setCargando(false)
-                return
-            }
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
 
-            const config = {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${token}`
-                }
-            }
+      try {
+        const { data } = await clienteAxios("/usuarios/perfil", config);
+        setAuth(data);
+        navigate("/proyectos");
+      } catch (error) {
+        setAuth({});
+      }
 
-            try {
-                const { data } = await clienteAxios('/usuarios/perfil', config);
-                setAuth(data);
-                navigate('/proyectos');
-            } catch (error) {
-                setAuth({})
-            }
+      setCargando(false);
+    };
+    autenticarUsuario();
+  }, []);
 
-            setCargando(false)
-        }
-        autenticarUsuario();
-    }, [])
-    
-    return (
-        <AuthContext.Provider
-            value={{
-                auth,
-                setAuth,
-                cargando
-            }}
-        >
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const cerrarSesionAuth = () => {
+    setAuth({});
+  };
 
-export {
-    AuthProvider
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+        cargando,
+        cerrarSesionAuth,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider };
 
 export default AuthContext;
